1e49b115c4cdce984fa6cb7ab1708a152817d16b
Batoulabdulhadi <batoulabdulhadi1@gmail.com>
2025-06-04 12:36:57 +0300

    Added (Delete Task Feature)

--- Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33110592e311afda8b28d93661ce66d16e544618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
